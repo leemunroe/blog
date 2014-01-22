66001ed977595afea64e18371daa080596e13862
Lee Munroe <lee@leemunroe.com>
2014-01-21 17:46:00 -0800

    Add and config grunt

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/*.js'
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        compass: {
+            dist: {
+              options: {
+                sassDir: 'css/sass',
+                cssDir: 'css',
+                environment: 'production'
+              }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify','compass']);
+
+};
