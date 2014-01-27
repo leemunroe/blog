17af87f4de57d6c9c2f415f7bc7729cbd3314e30
Lee Munroe <lee@leemunroe.com>
2014-01-26 16:25:18 -0800

    Styling blog and sidebar

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,12 +30,30 @@ module.exports = function(grunt) {
             }
         },
 
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['css/sass/*.scss'],
+                tasks: ['compass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        }
+
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
