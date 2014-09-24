2a504d74f544c7940b8182a5b8517e1a45e3f90c
Lee Munroe <lee@leemunroe.com>
2014-09-23 21:49:22 -0700

    Added additional subscribe action to post

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,6 @@ module.exports = function(grunt) {
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat','uglify','compass','jekyll:server','watch']);
+    grunt.registerTask('default', ['concat','uglify','compass','watch']);
 
 };
